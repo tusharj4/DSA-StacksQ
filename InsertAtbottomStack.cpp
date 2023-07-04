21b634edbe9de4402d30a715c984509ee5201fc9
Tushar Joshi <92170763+tusharj4@users.noreply.github.com>
2023-07-04 20:16:01 +0530

    Add files via upload

---
+++ InsertAtbottomStack.cpp
@@ -0,0 +1,26 @@
+//Insert an element at the bottom of a stack
+void ins(stack<int>& myStack, int target)
+{
+    if(myStack.empty())
+    {
+        myStack.push(target);
+        return;
+    }
+    //take out the top element and store it in the temporary variable and then pop
+    int temp=myStack.top();
+    myStack.pop();
+    ins(myStack,target);//recursive call so as to empty tthe stack
+    myStack.push(temp);//reverse the operations performed 
+}
+void pushAtBottom(stack<int>& myStack, int x) 
+{
+    // Write your code here.
+    if(myStack.empty())
+    {
+        cout<<"The stack is empty"<<endl;
+        return;
+        // return myStack ;
+    }
+    ins(myStack, x);
+    // return myStack;
+}
